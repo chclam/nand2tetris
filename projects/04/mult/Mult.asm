3501e5cfd9be573ed30f6e1b3226c7ffd3fcb937
Chris Lam <chris@chrislam.nl>
2023-05-17 16:02:23 +0200

    Add Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+  // R2 = 0
+  @R2
+  M=0
+
+(LOOP)
+  // if (R1 == 0) break;
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  // R2 = R2 + R0
+  @R0
+  D=M
+  @R2
+  M=M+D
+
+  // R1 = R1 - 1
+  @R1
+  M=M-1
+
+  // Jump back to LOOP
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
+  
